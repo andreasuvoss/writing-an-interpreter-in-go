9aeac61c012f3c6eeae481f8731343328f3ace2f
Andreas Voss <andreas@anvo.dk>
2024-05-10 10:29:09 +0200

    Added REPL

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"monkey/repl"
+)
+
+func main() {
+    user, err := user.Current()
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+    fmt.Print("Feel free to type in commands!\n", user.Username)
+    repl.Start(os.Stdin, os.Stdout)
+}
